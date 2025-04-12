46c65e608495c8d04893ec7f4432ddba97314d1e
sadraliev <a.sadraliev@gmail.com>
2025-04-12 03:57:05 -0400

    feat: add error handling to bot implementation

--- bot/bot.ts
+++ bot/bot.ts
@@ -1,4 +1,4 @@
-import { Bot } from "grammy";
+import { Bot, GrammyError, HttpError } from "grammy";
 import { getBotToken } from "./config";
 import { renderChatInfo } from "./handlers";
 
@@ -20,4 +20,16 @@ bot.on("message", (ctx) => {
   });
 });
 
+bot.catch((err) => {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error("Error in request:", e.description);
+  } else if (e instanceof HttpError) {
+    console.error("Could not contact Telegram:", e);
+  } else {
+    console.error("Unknown error:", e);
+  }
+});
 export { bot };
